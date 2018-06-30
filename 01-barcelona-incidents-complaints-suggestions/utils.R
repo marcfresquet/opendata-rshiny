10696e766bd16603052b0465a3f86b32bf7c1433
Marc Fresquet Rius <marc@driviesapp.com>
2018-06-30 20:25:11 +0200

    Read all data from folder

--- 01-barcelona-incidents-complaints-suggestions/utils.R
+++ 01-barcelona-incidents-complaints-suggestions/utils.R
@@ -1,7 +1,6 @@
 ############
 ### Packages
 ############
-
 library(R.utils)
 library(shinydashboard)
 library(shinyjs)
@@ -16,10 +15,33 @@ library(tidyr)
 ############
 ### Constants
 ############
+target_columns <- c(
+  "TIPUS",
+  "AREA",
+  "ELEMENT",
+  "DETALL",
+  "DIA_DATA_ALTA",
+  "MES_DATA_ALTA",
+  "ANY_DATA_ALTA",
+  "DIA_DATA_TANCAMENT",
+  "MES_DATA_TANCAMENT",
+  "ANY_DATA_TANCAMENT",
+  "DISTRICTE",
+  "BARRI",
+  "TIPUS_VIA",
+  "CARRER",
+  "NUMERO",
+  "LONGITUD",
+  "LATITUD",
+  "SUPORT",
+  "CANALS_RESPOSTA"
+)
 
 ############
-### Resources
+### Data
 ############
+filenames <- list.files("data", full.names = TRUE)
+data_barcelona <- Reduce(rbind, lapply(filenames, data_reader))
 
 ############
 ### Directory
