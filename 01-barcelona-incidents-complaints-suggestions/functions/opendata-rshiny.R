882e34cfb21a5a766c662ccf4f7e3cbd50bafe76
Marc Fresquet Rius <marc@driviesapp.com>
2018-07-07 11:07:12 +0200

    Check comma in values

--- 01-barcelona-incidents-complaints-suggestions/functions/opendata-rshiny.R
+++ 01-barcelona-incidents-complaints-suggestions/functions/opendata-rshiny.R
@@ -1,6 +1,8 @@
 format_selectize_input_values <- function(input_values) {
   # Remove blank spaces
   input_values <- gsub(" ", "_", input_values)
+  # Remove comma
+  input_values <- gsub(",", "COMMA", input_values)
   # Remove slash
   input_values <- gsub("/", "SLASH", input_values)
   # Remove apostrophe
@@ -10,6 +12,7 @@ format_selectize_input_values <- function(input_values) {
   # Return to original data
   input_values <- gsub("APOSTROPHE", "'", input_values)
   input_values <- gsub("SLASH", "/", input_values)
+  input_values <- gsub("COMMA", ",", input_values)
   input_values <- gsub("_", " ", input_values)
   input_values
 }
