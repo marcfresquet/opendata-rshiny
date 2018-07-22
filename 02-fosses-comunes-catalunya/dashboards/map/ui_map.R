e4d4835e0c51d605cc92156c6181fa48ccde542d
Marc Fresquet Rius <marc@driviesapp.com>
2018-07-22 11:54:40 +0200

    Add filter

--- 02-fosses-comunes-catalunya/dashboards/map/ui_map.R
+++ 02-fosses-comunes-catalunya/dashboards/map/ui_map.R
@@ -18,7 +18,8 @@ body_map <- tabItem(tabName = map_dashboard_id,
     column(3,
      wellPanel(
        h3("Filtres"),
-       selectizeInput("municipi_filter", "Municipi", unique(data_fosses$Municipi), multiple = TRUE, options = selectizeInput_opts),
+       selectizeInput("municipi_filter", "Municipi", sort(unique(data_fosses$Municipi)), multiple = TRUE, options = selectizeInput_opts),
+       selectInput("confirmada_filter", "Fossa confirmada", c("Mostra tot", unique(data_fosses$IdCategoria)), multiple = FALSE),
        selectInput("bandol_filter", "Bàndol", c("Mostra tot", "Republicà", "Rebel", "Ambdós bàndols"), multiple = FALSE),
        selectInput("tipus_fossa_filter", "Tipus Fossa", na.omit(c("Mostra tot" , unique(data_fosses$TipusFossa))), multiple = FALSE),
        selectInput("excavades_filter", "Excavades", c("Mostra tot" , unique(data_fosses$Excavades)), multiple = FALSE)
